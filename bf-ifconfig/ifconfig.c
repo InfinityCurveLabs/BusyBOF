aa3c679240e48d3accee348714f8d4de8ad64c66
cmprmsd <73472903+cmprmsd@users.noreply.github.com>
2026-05-06 15:34:52 +0200

    fix: add IPv6 address retrieval in ifconfig for network interfaces

--- bf-ifconfig/ifconfig.c
+++ bf-ifconfig/ifconfig.c
@@ -74,6 +74,35 @@ void go(char *args, int alen) {
             }
         }
 
+        /* IPv6 via /proc/net/if_inet6 */
+        FILE *f6 = fopen("/proc/net/if_inet6", "r");
+        if (f6) {
+            char line6[256];
+            while (fgets(line6, sizeof(line6), f6)) {
+                char hex6[33], iface6[IFNAMSIZ];
+                int idx, plen, scope, flags;
+                if (sscanf(line6, "%32s %x %x %x %x %s", hex6, &idx, &plen, &scope, &flags, iface6) == 6
+                    && strcmp(iface6, ent->d_name) == 0) {
+                    unsigned char addr6[16];
+                    for (int i = 0; i < 16; i++) {
+                        unsigned int b;
+                        sscanf(hex6 + i * 2, "%2x", &b);
+                        addr6[i] = (unsigned char)b;
+                    }
+                    char ip6[INET6_ADDRSTRLEN];
+                    inet_ntop(AF_INET6, addr6, ip6, sizeof(ip6));
+                    const char *scopestr = "Global";
+                    if (scope == 0x20) scopestr = "Link";
+                    else if (scope == 0x10) scopestr = "Host";
+                    else if (scope == 0x40) scopestr = "Admin";
+                    else if (scope == 0x80) scopestr = "Site";
+                    BeaconPrintf(CALLBACK_OUTPUT, "        inet6 %s  prefixlen %d  scopeid 0x%x<%s>\n",
+                                 ip6, plen, scope, scopestr);
+                }
+            }
+            fclose(f6);
+        }
+
         /* HW address */
         snprintf(flagpath, sizeof(flagpath), "/sys/class/net/%s/address", ent->d_name);
         fp = fopen(flagpath, "r");
